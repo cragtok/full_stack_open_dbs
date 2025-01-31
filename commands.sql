e5c0b56f7478ac067e08d9be21652dc4fd5ab65d
cragtok <lidfun@protonmail.com>
2025-01-31 14:15:59 +0600

    exercise 13.2

---
+++ commands.sql
@@ -0,0 +1,25 @@
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  author text,
+  url text NOT NULL,
+  title text NOT NULL,
+  likes int DEFAULT 0
+);
+
+INSERT INTO
+  blogs (author, url, title)
+values
+  (
+    'Dan Abramov',
+    'https://overreacted.io/on-let-vs-const/',
+    'On let vs const'
+  );
+
+INSERT INTO
+  blogs (author, url, title)
+values
+  (
+    'Laurenz Albe',
+    'https://www.cybertec-postgresql.com/en/gaps-in-sequences-postgresql/',
+    'Gaps in sequences in PostgreSQL'
+  );
