27dcacb15452d2d15989f8374432a19e7c25de7c
cragtok <lidfun@protonmail.com>
2025-02-09 04:58:48 +0600

    exercise 13.23

--- controllers/users.js
+++ controllers/users.js
@@ -46,6 +46,11 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
+    const where = {};
+    if (req.query.read) {
+        where.read = req.query.read === "true";
+    }
+
     const user = await User.findByPk(req.params.id, {
         include: [
             {
@@ -72,6 +77,7 @@ router.get("/:id", async (req, res) => {
                 through: {
                     as: "readinglists",
                     attributes: ["read", "id"],
+                    where
                 },
             },
             {
